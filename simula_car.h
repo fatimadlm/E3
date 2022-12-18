44455bc81e38a305e3cefb1bdfdef33a3912ef1a
fatimadlm <99411119+fatimadlm@users.noreply.github.com>
2022-12-18 21:23:42 +0100

    Añadido simula_car.h
    Cabecera, incluido en simular_car.c

--- simula_car.h
+++ simula_car.h
@@ -6,13 +6,22 @@
 #define N_COCHES 8
 
 pthread_mutex_t mutex = PTHREAD_MUTEX_INITIALIZER;
+
 /* Tipo de datos que representa un coche*/
 typedef struct {
     int id;
     char *cadena;
 } coche_t;
 
+int i;
+
 /* Array de datos de tipo coche_t*/
 coche_t coches[N_COCHES];
 
+/*Funcion en simular_car:*/
 void *funcion_coche(coche_t *pcoche);
+/*Array para guardar los ganadores*/
+int clasificacionFinal[N_COCHES];
+/*Registro de la siguiente posición vacia*/
+int finalCarrera;
+
